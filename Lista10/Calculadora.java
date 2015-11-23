a3f314ee1e1fe9b4172d33b0c9b9ae308fe56969
Eduardo <edu.ugolini2@hotmail.com>
2015-11-23 10:26:27 -0200

    Adicionado Exercicio3 da lista 10

--- Lista10/Calculadora.java
+++ Lista10/Calculadora.java
@@ -6,28 +6,27 @@ package br.edu.ifcvideira.Lista10;
 public class Calculadora extends PropriedadesAzulejo{
     private double areaParede, areaAzulejo;
 
-    public Calculadora(String tipoAzulejo){
-        this.setAreaParede();
-        this.setAreaAzulejo(tipoAzulejo);
+    public Calculadora(){
     }
 
-    private void setAreaAzulejo(String tipoAzulejo) {
-        if(tipoAzulejo.equals("quadrado")){
+    public void setAreaAzulejo(String tipoAzulejo) {
+        if(tipoAzulejo.equals("Quadrado")){
            this.areaAzulejo = this.getLarguraAzulejo() * this.getLarguraAzulejo();
         }
-        else if(tipoAzulejo.equals("retangular")){
+        else if(tipoAzulejo.equals("Retangular")){
             this.areaAzulejo = this.getLarguraAzulejo() * this.getAlturaAzulejo();
         }
-        else if (tipoAzulejo.equals("triangular de lados iguais")){
+        else if (tipoAzulejo.equals("Triangular de lados iguais")){
             this.areaAzulejo = (this.getLarguraAzulejo() * this.getLarguraAzulejo())/2;
         }
-        else if(tipoAzulejo.equals("triangular de lados diferentes")){
+        else if(tipoAzulejo.equals("Triangular de lados diferentes")){
             this.areaAzulejo = (this.getLarguraAzulejo() * this.getAlturaAzulejo())/2;
         }
+        setAreaParede();
     }
 
     private void setAreaParede(){
-        this.areaParede = this.getAlturaParede() + this.getLarguraParede();
+        this.areaParede = this.getAlturaParede() * this.getLarguraParede();
     }
 
     public double getQuantidadeAzulejos(){
